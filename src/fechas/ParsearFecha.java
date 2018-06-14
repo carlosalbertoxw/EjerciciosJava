6f2eb6a3f6f14f45f511a7ace582fa1e06e2d8ea
Carlos Alberto <carlosalbertoxw@gmail.com>
2018-06-13 23:43:33 -0500

    Se agregó otro formato de fecha

--- src/fechas/ParsearFecha.java
+++ src/fechas/ParsearFecha.java
@@ -8,6 +8,7 @@ package fechas;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,8 +42,21 @@ public class ParsearFecha {
         return f;
     }
 
+    public static String ddmmmyyyy(String f) {
+        if (f != null && f.length() > 0) {
+            try {
+                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(f);
+                return String.valueOf(new SimpleDateFormat("dd-MMM-yyyy", new Locale("es")).format(date));
+            } catch (ParseException ex) {
+                Logger.getLogger(ParsearFecha.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return f;
+    }
+
     public static void main(String[] args) {
         System.out.println(ParsearFecha.ddmmyyyy("1999-01-20"));
         System.out.println(ParsearFecha.yyyymmdd("20-01-1999"));
+        System.out.println(ParsearFecha.ddmmmyyyy("1999-01-20"));
     }
 }

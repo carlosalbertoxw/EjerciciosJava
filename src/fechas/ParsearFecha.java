57f007069207ea331ce3fbd59d980fd27ff67533
Carlos Alberto <carlosalbertoxw@gmail.com>
2018-06-13 23:25:44 -0500

    Se agregó paquete de fechas

---
+++ src/fechas/ParsearFecha.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fechas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ParsearFecha {
+
+    public static String yyyymmdd(String f) {
+        if (f != null && f.length() > 0) {
+            try {
+                Date date = new SimpleDateFormat("dd-MM-yyyy").parse(f);
+                return String.valueOf(new SimpleDateFormat("yyyy-MM-dd").format(date));
+            } catch (ParseException ex) {
+                Logger.getLogger(ParsearFecha.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return f;
+    }
+
+    public static String ddmmyyyy(String f) {
+        if (f != null && f.length() > 0) {
+            try {
+                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(f);
+                return String.valueOf(new SimpleDateFormat("dd-MM-yyyy").format(date));
+            } catch (ParseException ex) {
+                Logger.getLogger(ParsearFecha.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ParsearFecha.ddmmyyyy("1999-01-20"));
+        System.out.println(ParsearFecha.yyyymmdd("20-01-1999"));
+    }
+}

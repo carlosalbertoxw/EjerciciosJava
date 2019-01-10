052b2654c08fe20a759f5f58b1dbe9ef4434b2a3
Carlos Alberto <carlosalbertoxw@gmail.com>
2019-01-10 00:55:25 -0600

    Mejora en la función de validación

--- src/utilidades/ValidarInputs.java
+++ src/utilidades/ValidarInputs.java
@@ -14,74 +14,92 @@ import java.util.regex.Pattern;
  */
 public class ValidarInputs {
 
-    public static final String VI_USERNAME = "username";
+    public static final String VI_URI = "uri";
     public static final String VI_URL = "url";
     public static final String VI_NUMERIC = "numeric";
     public static final String VI_NUMERIC_DECIMAL = "numeric_decimal";
     public static final String VI_EMAIL = "email";
     public static final String VI_STRING = "string";
 
-    public Boolean valInput(String val, String type, Integer length, Boolean empty) {
-        return tle(val, type, length, empty);
+    public String valInput(String val, String type, Integer length, Boolean empty, String field) {
+        return tle(val, type, length, empty, field);
     }
 
-    private Boolean tle(String val, String type, Integer length, Boolean empty) {
+    private String tle(String val, String type, Integer length, Boolean empty, String field) {
         if (val == null) {
-            return false;
+            return "El campo '" + field + "' no puede ser nulo";
         }
         if (empty == false && val.length() == 0) {
-            return false;
+            return "El campo '" + field + "' no puede estar vacio";
         } else if (empty == false && val.length() > 0) {
-            return t(val, type, length);
+            return t(val, type, length, field);
         } else if (empty == true && val.length() > 0) {
-            return t(val, type, length);
+            return t(val, type, length, field);
         } else {
-            return empty == true && val.length() == 0;
+            return "";
         }
     }
 
-    private Boolean t(String val, String type, Integer length) {
-        Boolean flag;
+    private String t(String val, String type, Integer length, String field) {
+        String response;
         String pattern;
+        String mensaje;
         switch (type) {
             case "uri":
                 pattern = "^[0-9a-zA-Z-]*$";
-                flag = val(val, pattern);
+                mensaje = "El campo '" + field + "' solo acepta los siguientes caracteres 0-9 a-z A-Z - ";
+                response = val(val, pattern, mensaje);
                 break;
             case "url":
                 pattern = "^(http|https)\\://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,3}(/\\S*)?$";
-                flag = val(val, pattern);
+                mensaje = "Ingresa una URL valida en el campo '" + field + "'";
+                response = val(val, pattern, mensaje);
                 break;
             case "numeric":
                 pattern = "^[0-9]*$";
-                flag = val(val, pattern);
+                mensaje = "Ingresa un valor numérico valido en el campo '" + field + "'";
+                response = val(val, pattern, mensaje);
                 break;
             case "numeric_decimal":
                 pattern = "^[0-9,]*\\.?[0-9]*$";
-                flag = val(val, pattern);
+                mensaje = "Ingresa un valor decimal valido en el campo '" + field + "'";
+                response = val(val, pattern, mensaje);
                 break;
             case "email":
                 pattern = "^[^@\\s]+@[^@\\.\\s]+(\\.[^@\\.\\s]+)+$";
-                flag = val(val, pattern);
+                mensaje = "Ingresa un correo electrónico valido en el campo '" + field + "'";
+                response = val(val, pattern, mensaje);
                 break;
             case "string":
-                flag = true;
+                response = "";
                 break;
             default:
-                flag = false;
+                response = "Error al validar el tipo de dato del campo '" + field + "'";
                 break;
         }
-        return l(val, flag, length);
+        return l(val, response, length, field);
     }
 
-    private Boolean l(String val, Boolean flag, Integer length) {
-        return flag && val.length() <= length;
+    private String l(String val, String response, Integer length, String field) {
+        if (response.equals("")) {
+            if (val.length() <= length) {
+                return "";
+            } else {
+                return "La longitud del campo '" + field + "' no puede ser mayor a " + length;
+            }
+        } else {
+            return response;
+        }
     }
 
-    public Boolean val(String val, String pattern) {
+    public String val(String val, String pattern, String message) {
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(val);
-        return m.find();
+        if (m.find()) {
+            return "";
+        } else {
+            return message;
+        }
     }
 
 }
